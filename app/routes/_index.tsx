c16738890b1871ff5489c45cf9218f3455476fae
minako yoshino <minako.yoshino@kaminashi.com>
2024-07-24 19:27:24 +0900

    add: terraform

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Remix Quiz!</h1>
+      <h1 className="text-3xl">Remix Quiz! with Terraform</h1>
       <ul className="list-disc mt-4 pl-6 space-y-2">
         <li>
           <a
